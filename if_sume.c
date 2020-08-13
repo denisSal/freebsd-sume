denisSal/freebsd-sume

commit 896285bd3be8e804ea27e038877c4e6e904ba332

Save space with better conditions

if_sume.c
@@ -1130,19 +1130,14 @@ sume_if_start(struct ifnet *ifp)
 	KASSERT(nf_priv->riffa_channel == SUME_RIFFA_CHANNEL_DATA,
 	    ("TX on non-data channel"));
 
-	if (!adapter->running)
-		return;
-	if (!(ifp->if_flags & IFF_UP))
+	if (!adapter->running && !(ifp->if_flags & IFF_UP))
 		return;
 
 	SUME_LOCK(adapter);
-	if (adapter->send[SUME_RIFFA_CHANNEL_DATA]->state !=
+	if (adapter->send[SUME_RIFFA_CHANNEL_DATA]->state ==
 	    SUME_RIFFA_CHAN_STATE_IDLE) {
-		SUME_UNLOCK(adapter);
-		return;
+		sume_if_start_locked(ifp);
 	}
-
-	sume_if_start_locked(ifp);
 	SUME_UNLOCK(adapter);
 }
 

commit b21f79f759176bc8dd9fac9e414433f9e459cd31

Fix bug introduced in 896285bd

if_sume.c
@@ -1130,7 +1130,7 @@ sume_if_start(struct ifnet *ifp)
 	KASSERT(nf_priv->riffa_channel == SUME_RIFFA_CHANNEL_DATA,
 	    ("TX on non-data channel"));
 
-	if (!adapter->running && !(ifp->if_flags & IFF_UP))
+	if (!adapter->running || !(ifp->if_flags & IFF_UP))
 		return;
 
 	SUME_LOCK(adapter);